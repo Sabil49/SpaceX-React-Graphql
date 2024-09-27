5d1830fa2bee6abf295499f9d1f6d37f35581223
Sabil49 <md.sabeel10@gmail.com>
2024-09-27 21:09:10 +0530

    spacex app in react & graphql

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,43 @@
-require('dotenv').config();
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require("./utils"); //highlight-line
+const resolvers = require("./resolvers"); 
+//highlight-start
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+//highlight-end
+
+const store = createStore(); //highlight-line
+const isEmail = require("isemail"); 
+const server = new ApolloServer({
+    context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+
+    if (!isEmail.validate(email)) return { user: null };
+
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+
+    return { user: { ...user.dataValues } };
+  },
+  typeDefs,
+  resolvers,
+  //highlight-start
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+  //highlight-end
+});
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/sandbox
+  `);
+});
